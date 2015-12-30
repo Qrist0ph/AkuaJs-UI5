b5538ab6c2c1f5bedfeccca8fe5a3070880c1ce3
Christoph Außem <chris.aussem@gmail.com>
2015-12-30 10:28:59 +0100

    AkuaJs UI5

--- BarChart.js
+++ BarChart.js
@@ -1,6 +1,4 @@
  jQuery.sap.declare("AkuaJs.BarChart"); 
-//  jQuery.sap.require("AkuaJs.Core");
-
 
    
   sap.ui.core.Control.extend("AkuaJs.BarChart", {  
@@ -8,41 +6,29 @@
                 oRm.write("<div");  
                 oRm.writeControlData(oControl);  
                 oRm.write("</div>");  
-            },  
-            onAfterRendering: function (eeee) {  
+            }, 
+             metadata : {  
+    properties: {  
+      "axis0": {type : "any"},
+      "slicer": {type : "any"},  
+      "connection": {type : "any"},  
+      "colors": {type : "string[]"},  
+      "numberFormat": {type : "string",defaultValue: ',.0f'},
+    }}, 
+            onAfterRendering: function (event) {  
+			var me = this;
+			 require(["BarChartNvd3"], function(chart) {
 			
-			 require(["BarChartNvd3"], function(TestChart) {
-				eJune = E(D("Month"), "June");
-				eAugust = E(D("Month"), "August");
-				ePoland = E(D("Country"), "Poland");        
-				eCar = E(D("Product"), "Car");
-				eBike = E(D("Product"), "Bike");
-				eBoat = E(D("Product"), "Boat");
-				eCost = E(D("Measure"), "Costs");
-				eRevenue = E(D("Measure"), "Revenue");
-
-				barchart = TestChart({
-					axis0: A({
-						crosslists:
-						[
-							TCL([
-								T([eCar]), T([eBike]), T([eBoat])
-							]),
-							TCL([
-								T([eAugust])
-							])
-						]
-					}),
-					slicer: T([eCost]),
-
-					Connection : LocalCubeConnection(HyperCube(SDTL([ 
-								 T([eAugust, eCar, eCost],440)
-								,T([eAugust, eBike, eCost],77)    
-								,T([eAugust, eBoat, eCost],120)])))
+				var barchart = chart({
+					axis0: me.getAxis0(),
+					slicer: me.getSlicer(),
+					Connection : me.getConnection(),
+                    colors:me.getColors(),
+                    numberFormat : me.getNumberFormat()
 
 				});
 
-				barchart.getViewCaller(eeee.srcControl.$());
+				barchart.getViewCaller(event.srcControl.$());
 				
 			});
                
