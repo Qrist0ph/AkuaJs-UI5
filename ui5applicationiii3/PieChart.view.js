196311d0fe406f867cea8fa3af2cf3eb7f64af65
Christoph Außem <chris.aussem@gmail.com>
2016-01-03 12:17:24 +0100

    SAP UI5

--- ui5applicationiii3/PieChart.view.js
+++ ui5applicationiii3/PieChart.view.js
@@ -4,14 +4,11 @@ sap.ui.jsview("ui5applicationiii3.PieChart", {
 
         jQuery.sap.require("AkuaJs.PieChart");
 
-        eJune = E(D("Month"), "June");
         eAugust = E(D("Month"), "August");
-        ePoland = E(D("Country"), "Poland");
         eCar = E(D("Product"), "Car");
         eBike = E(D("Product"), "Bike");
         eBoat = E(D("Product"), "Boat");
         eCost = E(D("Measure"), "Costs");
-        eRevenue = E(D("Measure"), "Revenue");
 
         return new sap.m.Page({
             title: "Pie Chart",
@@ -29,15 +26,7 @@ sap.ui.jsview("ui5applicationiii3.PieChart", {
              }),
                  new AkuaJs.PieChart({
                      axis0: A({
-                         crosslists:
-                         [
-                             TCL([
-                                 T([eCar]), T([eBike]), T([eBoat])
-                             ]),
-                             TCL([
-                                 T([eAugust])
-                             ])
-                         ]
+                         crosslists: [TCL([T([eCar]), T([eBike]), T([eBoat])]), ]
                      }),
                      slicer: T([eCost]),
                      connection: [
@@ -45,9 +34,33 @@ sap.ui.jsview("ui5applicationiii3.PieChart", {
                                  , T([eAugust, eBike, eCost], 77)
                                  , T([eAugust, eBoat, eCost], 120)],
                      colors: ["#FFD700", "#F4A460", "#990000"],
-                     numberFormat: ',.2'
+                     numberFormat: ',.2',
                  }),
-
+                 new sap.ui.core.HTML({
+                     content: ' '
+                     + '<pre id="sss"><code>'
+                     +'jQuery.sap.require("AkuaJs.PieChart");<br/>'
++'<br/>'
++'eAugust = E(D("Month"), "August");<br/>'
++'eCar = E(D("Product"), "Car");<br/>'
++'eBike = E(D("Product"), "Bike");<br/>'
++'eBoat = E(D("Product"), "Boat");<br/>'
++'eCost = E(D("Measure"), "Costs");<br/>'
++'new AkuaJs.PieChart({<br/>'
++'    axis0: A({<br/>'
++'        crosslists: [TCL([T([eCar]), T([eBike]), T([eBoat])]), ]<br/>'
++'    }),<br/>'
++'    slicer: T([eCost]),<br/>'
++'    connection: [<br/>'
++'                 T([eAugust, eCar, eCost], 440)<br/>'
++'                , T([eAugust, eBike, eCost], 77)<br/>'
++'                , T([eAugust, eBoat, eCost], 120)],<br/>'
++'    colors: ["#FFD700", "#F4A460", "#990000"],<br/>'
++'    numberFormat: ",.2",<br/>'
++'$})<br/>'
+                     + '</code></pre>'
+                     + '<script> $("pre code").each(function(i, block) {hljs.highlightBlock(block);});</script>'
+                 })
             ]
         });
     }
